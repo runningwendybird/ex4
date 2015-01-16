f6adb13eaac839f31a2fe7ef644bfe6ac9264f06
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 18:01:12 -0800

    Part 1 Tests Pass

--- list_operations.py
+++ list_operations.py
@@ -25,28 +25,28 @@ def head(input_list):
         Return the first element of the input list.
         [ A, B, C, D, E, F ] --> A
     """
-    pass
+    return input_list[0]
 
 def tail(input_list):
     """
         Return all elements of the input list except the first.
         [ A, B, C, D ] --> [ B, C, D ]
     """
-    pass
+    return input_list[1:]
 
 def last(input_list):
     """
         Return the last element of the input list.
         [ A, B, C, D ] --> D
     """
-    pass
+    return input_list[-1]
 
 def init(input_list):
     """
         Return all elements of the input list except the last.
         [ A, B, C, D ] --> [ A, B, C ]
     """
-    pass
+    return input_list[:-1]
 
 """
 Do yourself a favor and get a short code review here.
@@ -58,21 +58,21 @@ def first_three(input_list):
         Return the first three elements of the input list.
         [ A, B, C, D, E, F ] --> [ A, B, C ]
     """
-    pass
+    return input_list[0:3]
 
 def last_five(input_list):
     """
         Return the last five elements of the input list.
         [ A, B, C, D, E, F ] --> [ B, C, D, E, F ]
     """
-    pass
+    return input_list[-5:]
 
 def middle(input_list):
     """
         Return all elements of the input list except the first two and the last two.
         [ A, B, C, D, E, F ] --> [ C, D ]
     """
-    pass
+    return input_list[2:-2]
 
 def inner_four(input_list):
     """
@@ -80,7 +80,7 @@ def inner_four(input_list):
         [ A, B, C, D, E, F, G ] --> [ C, D, E, F ]
     """
 
-    pass
+    return input_list[2:6]
 
 def inner_four_end(input_list):
     """
@@ -88,21 +88,24 @@ def inner_four_end(input_list):
         list, in that order.
         [ A, B, C, D, E, F, G, H, I, J, K, L] --> [ G, H, I, J ]
     """
-    pass
+    return input_list[-6:-2]
 
 def replace_head(input_list):
     """
         Replace the head of the input list with the value 42.
         [ A, B, C, D ] --> [ 42, B, C, D]
     """
-    pass
+    input_list[0] = 42
+    return input_list
 
 def replace_third_and_last(input_list):
     """
         Replace the third and last elements of the input list with the value 37.
         [ A, B, C, D, E, F ] --> [ A, B, 37, D, E, 37 ]
     """
-    pass
+    input_list[2] = 37
+    input_list[-1]= 37
+    return input_list
 
 def replace_middle(input_list):
     """
@@ -111,14 +114,18 @@ def replace_middle(input_list):
         [ A, B, C, D, E, F, G, H, I ] --> [ A, B, 42, 37, H, I ] 
     """
 
-    pass
+    input_list[2: -2] = [42, 37]
+    return input_list
 
 def delete_third_and_seventh(input_list):
     """
         Remove the third and seventh elements of the input list.
         [ A, B, C, D, E, F, G, H ] --> [ A, B, D, E, F, H ]
     """
-    pass
+    input_list[6:7] = []
+    input_list[2:3] = []
+
+    return input_list
 
 def delete_middle(input_list):
     """
@@ -126,7 +133,9 @@ def delete_middle(input_list):
         last two.
          [ A, B, C, D, E, F, G, H ] --> [ A, B, G, H ]
     """
-    pass
+    input_list[2:-2] = []
+
+    return input_list
 
 """
 Part 1 is finished! Ask for a code review before proceeding to Part 2.
